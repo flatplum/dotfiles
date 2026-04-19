53f96a65513739fc009a0f78d358bda48b4b98f5
flatplum <flatplum@proton.me>
2026-04-19 23:25:31 +1000

    chore: add comment to clipboard sync

--- nvim/dot-config/nvim/lua/options.lua
+++ nvim/dot-config/nvim/lua/options.lua
@@ -1,3 +1,5 @@
+-- Sync clipboard between OS and Neovim. Schedule the setting after `UiEnter`
+-- because it can increase startup-time.
 vim.api.nvim_create_autocmd('UIEnter', {
         callback = function()
                 vim.o.clipboard = 'unnamedplus'
