959bd8c4cceb6787982e4c1bac923a82ad68dd2e
flatplum <flatplum@proton.me>
2026-07-22 14:31:46 +1000

    feat: add custom component highlight support

--- nvim/dot-config/nvim/after/lsp/vtsls.lua
+++ nvim/dot-config/nvim/after/lsp/vtsls.lua
@@ -24,4 +24,13 @@ return {
                 },
         },
         filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+        -- See https://github.com/vuejs/language-tools/wiki/Neovim
+        on_attach = function(client)
+                local existing_capabilities = client.server_capabilities
+                if vim.bo.filetype == 'vue' then
+                        existing_capabilities.semanticTokensProvider.full = false
+                else
+                        existing_capabilities.semanticTokensProvider.full = true
+                end
+        end
 }
