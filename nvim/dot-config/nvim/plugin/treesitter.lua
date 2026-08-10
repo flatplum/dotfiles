bb75223dbfaae7b15efbadce782349a5f717e981
flatplum <flatplum@proton.me>
2026-08-11 01:56:58 +1000

    feat: enable buffer highlight support

--- nvim/dot-config/nvim/plugin/treesitter.lua
+++ nvim/dot-config/nvim/plugin/treesitter.lua
@@ -11,3 +11,13 @@ require('nvim-treesitter').install({
         'yaml',
         'julia',
 })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+        callback = function(args)
+                vim.schedule(function()
+                        if vim.api.nvim_buf_is_valid(args.buf) then
+                                pcall(vim.treesitter.start, args.buf)
+                        end
+                end)
+        end,
+})
